00f5c1ae2668241faa410103bfaf7caa7744f98d
KhaledBouden <khaledbouden15@gmail.com>
2025-07-30 12:43:38 +0100

    hotfix

--- app/scrapbook/[id]/page.tsx
+++ app/scrapbook/[id]/page.tsx
@@ -1,6 +1,14 @@
+// app/scrapbook/[id]/page.tsx or app/album/[id]/page.tsx
 
 import AlbumDetail from './AlbumDetail';
 
+interface AlbumPageProps {
+  params: {
+    id: string;
+  };
+}
+
+// This function tells Next.js which dynamic routes to pre-render at build time
 export async function generateStaticParams() {
   return [
     { id: '1' },
@@ -12,6 +20,7 @@ export async function generateStaticParams() {
   ];
 }
 
-export default function AlbumPage({ params }: { params: { id: string } }) {
+// The page component
+export default function AlbumPage({ params }: AlbumPageProps) {
   return <AlbumDetail albumId={params.id} />;
 }
